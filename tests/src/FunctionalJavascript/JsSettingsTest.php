9e92664e27469b2d8effd15a8dc47a384a14f386
tuutti <tuutti@iki.fi>
2022-10-14 11:57:23 +0300

    UHF-7109: Test drupalSettings

---
+++ tests/src/FunctionalJavascript/JsSettingsTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\helfi_navigation\FunctionalJavascript;
+
+use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
+use Drupal\helfi_api_base\Environment\EnvironmentResolver;
+use Drupal\helfi_api_base\Environment\Project;
+use Drupal\language\Entity\ConfigurableLanguage;
+
+/**
+ * Tests drupal settings.
+ *
+ * @group helfi_navigation
+ */
+class JsSettingsTest extends WebDriverTestBase {
+
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'language',
+    'helfi_api_base',
+    'helfi_navigation',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() : void {
+    parent::setUp();
+
+    foreach (['fi', 'sv'] as $langcode) {
+      ConfigurableLanguage::createFromLangcode($langcode)->save();
+    }
+    $config = $this->config('helfi_api_base.environment_resolver.settings');
+    $config->set(EnvironmentResolver::ENVIRONMENT_NAME_KEY, 'local');
+    $config->set(EnvironmentResolver::PROJECT_NAME_KEY, Project::ASUMINEN);
+    $config->save();
+
+    $this->config('language.negotiation')
+      ->set('url.prefixes', ['en' => 'en', 'fi' => 'fi', 'sv' => 'sv'])
+      ->save();
+  }
+
+  /**
+   * Tests drupalSettings.
+   */
+  public function testSettings() : void {
+    foreach (['en', 'fi', 'sv'] as $language) {
+      $this->drupalGet('/' . $language);
+
+      $settings = $this->getDrupalSettings();
+      $this->assertEquals("https://helfi-etusivu.docker.so/$language/api/v1/global-menu", $settings['helfi_navigation']['links']['api']);
+      // @todo Fix once Etusivu instance is live.
+      $this->assertEquals("https://www.hel.fi", $settings['helfi_navigation']['links']['canonical']);
+    }
+  }
+
+}
