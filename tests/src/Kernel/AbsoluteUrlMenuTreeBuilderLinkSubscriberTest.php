b991246cd5428c8f49e235f97a6cffe02f838504
tuutti <tuutti@iki.fi>
2024-03-19 10:46:31 +0200

    UHF-9824: Added test

---
+++ tests/src/Kernel/AbsoluteUrlMenuTreeBuilderLinkSubscriberTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\helfi_navigation\Kernel;
+
+use Drupal\helfi_api_base\Environment\EnvironmentEnum;
+use Drupal\helfi_api_base\Environment\Project;
+use Drupal\Tests\helfi_api_base\Traits\EnvironmentResolverTrait;
+
+/**
+ * Tests absolute url subscriber.
+ *
+ * @group helfi_navigation
+ */
+class AbsoluteUrlMenuTreeBuilderLinkSubscriberTest extends MenuTreeBuilderTestBase {
+
+  use EnvironmentResolverTrait;
+
+  /**
+   * Tests that URLs are converted to absolute when the domains don't match.
+   *
+   * @dataProvider absoluteUrlData
+   */
+  public function testAbsoluteUrl(string $langcode, EnvironmentEnum $environment, string $expected) : void {
+    $this->setActiveProject(Project::ASUMINEN, $environment);
+    $menuTree = $this->getMenuTree($langcode);
+    $this->assertEquals($expected, $menuTree['url']);
+  }
+
+  /**
+   * A data provider.
+   *
+   * @return array[]
+   *   The data.
+   */
+  public function absoluteUrlData() : array {
+    return [
+      // The URL should be absolute because the local environment has
+      // a project-specific domains.
+      [
+        'fi',
+        EnvironmentEnum::Local,
+        'http://localhost/fi',
+      ],
+      // The URL should be relative because the prod environment shares
+      // the domain name (www.hel.fi).
+      [
+        'en',
+        EnvironmentEnum::Prod,
+        '/en',
+      ],
+    ];
+  }
+
+}
